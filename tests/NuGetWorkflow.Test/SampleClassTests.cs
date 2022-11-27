383dbf7d94970ceb3ce9ccfc14c151f71b2972bc
Rich Tebb <rich@tebb.io>
2022-11-27 15:57:05 +0000

    Add test comment

--- tests/NuGetWorkflow.Test/SampleClassTests.cs
+++ tests/NuGetWorkflow.Test/SampleClassTests.cs
@@ -8,10 +8,13 @@ public class SampleClassTests
     [Fact]
     public void can_get_a_number()
     {
+        // *** ARRANGE ***
         var sut = new SampleClass();
 
+        // *** ACT ***
         var result = sut.GetANumber();
 
+        // *** ASSERT ***
         result.Should().Be(2);
     }
 }
